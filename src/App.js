a130c1bce2b9d421a48e14075a7d1409daf18fba
PatrykJarzynka <patryk.jarzynka1234@gmail.com>
2022-06-27 01:21:04 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Newddss <code>src/App.js</code> and save to reload.
+          Newds <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
